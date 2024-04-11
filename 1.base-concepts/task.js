e364e60368d4254355ebc83b389555877b4088e6
migbaranes <migbaranes@gmail.com>
2024-04-11 21:49:31 +0300

    change#3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,11 +8,8 @@ function solveEquation(a, b, c) {
     if(D == 0) {
       arr.push(D = (-b / (2 * a)));
     } else if(D > 0){  
-      
-      let tmp = [];
-    arr.push(tmp);
-      tmp.push((-b + Math.sqrt(D)) / (2 * a)); 
-      tmp.push((-b - Math.sqrt(D)) / (2 * a));
+      arr.push((-b + Math.sqrt(D)) / (2 * a), 
+      (-b - Math.sqrt(D)) / (2 * a));
     }
 
   return arr;
@@ -23,12 +20,10 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let loanBody = amount - contribution;
 
   let amountMonths = loanBody * (perc + (perc / ((Math.pow((1 + perc), countMonths)) - 1)));
-  return  (+(amountMonths * countMonths)).toFixed(2);
+  return  (+((amountMonths * countMonths)).toFixed(2));
 }
 console.log(calculateTotalMortgage(10, 0, 50000, 12));
 console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-// console.log(calculateTotalMortgage(0, 0, 20000, 24));
 console.log(calculateTotalMortgage(10, 20000, 20000, 48));
-// console.log(calculateTotalMortgage(10, 20000, 20000, 24));
 console.log(calculateTotalMortgage(10, 0, 10000, 36));
 console.log(calculateTotalMortgage(15, 0, 10000, 36));
