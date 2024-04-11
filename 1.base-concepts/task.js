890f6088b733896f72f02b47b07e637f1f084e81
migbaranes <migbaranes@gmail.com>
2024-04-11 20:33:14 +0300

    change#2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let D = Math.pow(b, 2) - 4 * a * c;
-
+  
   let arr = [];
-
+  let D = Math.pow(b, 2) - 4 * a * c;
       if(D < 0) 
-  arr.push('');
+  arr.push();
     if(D == 0) {
       arr.push(D = (-b / (2 * a)));
-    } else if(D > 0){
+    } else if(D > 0){  
+      
       let tmp = [];
+    arr.push(tmp);
       tmp.push((-b + Math.sqrt(D)) / (2 * a)); 
       tmp.push((-b - Math.sqrt(D)) / (2 * a));
     }
@@ -19,15 +20,15 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let perc = (percent / 12) / 100;
-      loanBody = amount - contribution;
-      
-      monthlyPayment = loanBody * (perc + (perc / ((Math.pow((1 + perc), countMonths)) - 1)));
-  return (+(monthlyPayment * countMonths)).toFixed(2);
+  let loanBody = amount - contribution;
+
+  let amountMonths = loanBody * (perc + (perc / ((Math.pow((1 + perc), countMonths)) - 1)));
+  return  (+(amountMonths * countMonths)).toFixed(2);
 }
 console.log(calculateTotalMortgage(10, 0, 50000, 12));
 console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-console.log(calculateTotalMortgage(0, 0, 20000, 24));
-console.log(calculateTotalMortgage(10, 1000, 20000, 24));
-console.log(calculateTotalMortgage(10, 20000, 20000, 24));
+// console.log(calculateTotalMortgage(0, 0, 20000, 24));
+console.log(calculateTotalMortgage(10, 20000, 20000, 48));
+// console.log(calculateTotalMortgage(10, 20000, 20000, 24));
 console.log(calculateTotalMortgage(10, 0, 10000, 36));
 console.log(calculateTotalMortgage(15, 0, 10000, 36));
