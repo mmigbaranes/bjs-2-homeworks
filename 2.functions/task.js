b1927bb53e3f42f24318a804092e00fe5089462e
migbaranes <migbaranes@gmail.com>
2024-04-15 21:20:58 +0300

    Task change#1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,28 @@
 function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((acc, c) => acc + c);
+  let avg = (+(sum /arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+// function summElementsWorker(...arr) {
 
-}
+// }
 
-function differenceMaxMinWorker(...arr) {
+// function differenceMaxMinWorker(...arr) {
 
-}
+// }
 
-function differenceEvenOddWorker(...arr) {
+// function differenceEvenOddWorker(...arr) {
 
-}
+// }
 
-function averageEvenElementsWorker(...arr) {
+// function averageEvenElementsWorker(...arr) {
 
-}
+// }
 
-function makeWork (arrOfArr, func) {
+// function makeWork (arrOfArr, func) {
 
-}
+// }
