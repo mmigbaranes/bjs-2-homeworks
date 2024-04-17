5b2db4edc6a6926e16d900c2900d25702fc2f60a
migbaranes <migbaranes@gmail.com>
2024-04-17 12:14:02 +0300

    Task_arrays change#1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,6 @@
 function compareArrays(arr1, arr2) {
     const compareArrays = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);
+    return compareArrays(arr1, arr2);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
