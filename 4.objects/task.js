db859bca5f31124dcc73be7d4737b6b7965ecce5
migbaranes <migbaranes@gmail.com>
2024-04-19 20:58:54 +0300

    Task_change#1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
+  this.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  };
+  this.addMarks = function(...addToMarks) {
+    if (!this.marks) {
+        console.log("Error: Student has been expelled.");
+        return;
+    }
+    this.marks.push(...addToMarks);
+  };
+  this.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    const sum = this.marks.reduce((acc, c) => acc + c);
+    return sum / this.marks.length;
+  };
+  this.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason;
+  };
+
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
