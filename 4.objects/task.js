07f2005f70ab44445d469162ac4e42bd6768853e
migbaranes <migbaranes@gmail.com>
2024-04-19 22:42:35 +0300

    Task_change#2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,31 +1,32 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
-
-  this.setSubject = function(subjectName) {
-    this.subject = subjectName;
-  };
-  this.addMarks = function(...addToMarks) {
-    if (!this.marks) {
-        console.log("Error: Student has been expelled.");
-        return;
-    }
-    this.marks.push(...addToMarks);
-  };
-  this.getAverage = function() {
-    if (!this.marks || !this.marks.length) {
-        return 0;
-    }
-    const sum = this.marks.reduce((acc, c) => acc + c);
-    return sum / this.marks.length;
-  };
-  this.exclude = function(reason) {
-    delete this.subject;
-    delete this.marks;
-    this.exclude = reason;
-  };
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  
+    this.setSubject = function(subjectName) {
+      this.subject = subjectName;
+    };
+    
+    this.addMarks = function(...addToMarks) {
+      if (!this.marks) {
+          console.log("Ошибка: Студент был отчислен");
+          return;
+      }
+      this.marks.push(...addToMarks);
+    };
+    
+    this.getAverage = function() {
+      if (!this.marks || !this.marks.length) {
+          return 0;
+      }
+      const sum = this.marks.reduce((acc, c) => acc + c);
+      return sum / this.marks.length;
+    };
+    
+    this.exclude = function(reason) {
+      delete this.subject;
+      delete this.marks;
+      this.exclude = reason;
+    };
 }
-
