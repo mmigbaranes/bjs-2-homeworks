3b6e743bc6dd8c821275f840869fcbd78e564564
migbaranes <migbaranes@gmail.com>
2024-04-23 23:29:23 +0300

    Task_change#1

---
+++ 5.classes/task.js
@@ -0,0 +1,66 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount){
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+    }
+  
+    fix(){
+        this.state = this.state * 1.5;
+    }
+
+    set state(state){
+      if(state < 0){
+        this._state = 0;
+      } else if(state > 100){
+        this.state = 100;
+      } else {
+        this._state = state;
+      }
+    }
+    get state(){
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount){
+      super(name, releaseDate, pagesCount);
+          this.type = "magazine";
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+      constructor(author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+          this.type = "book";
+          this.author = author;
+      }
+  }
+  
+  class NovelBook extends Book {
+      constructor(author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+          this.type = "novel";
+          this.author = author;
+        }
+  }
+  
+  class FantasticBook extends Book {
+      constructor(author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+          this.author = author;
+          this.type = "fantastic";
+          }
+  }
+  
+  class DetectiveBook extends Book {
+      constructor(author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+          this.state = 100;
+          this.author = author;
+          this.type = "detective";
+          }
+  }
