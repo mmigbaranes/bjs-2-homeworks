a7d2cef924bf02a012a3daa876b456ebb3dc0c13
migbaranes <migbaranes@gmail.com>
2024-04-25 23:50:07 +0300

    Task_change#1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,14 @@
-﻿
+﻿function parseCount(arg){
+    let count = Number.parseFloat(arg);
+        if (Number.isNaN(count)){
+            throw new Error("Невалидное значение");
+        }
+        return +(count);
+}
+function validateCount(){
+    try{
+        return parseCount(arg);
+    }catch(error){
+        return error;
+    }
+}
