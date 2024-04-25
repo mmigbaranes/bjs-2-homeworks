28c07134cd3bd4ba48677d28e17e2088ac3dcfe0
migbaranes <migbaranes@gmail.com>
2024-04-26 01:22:44 +0300

    Task_change#3

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -12,3 +12,35 @@ function validateCount(arg){
         return error;
     }
 }
+
+class Triangle{
+    constructor(a,b,c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(a+b<c || a+c<b || b+c<a){
+            throw new Error("Треугольник c такими сторонами не существует");
+        }
+    }
+    get perimeter(){
+        return a+b+c;
+    }
+    get area(){
+        let p = (a+b+c)/2;
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+  }
+    function getTriangle(a,b,c){
+      try{
+        return new Triangle(a,b,c);
+      }catch(error){
+        return {
+          get perimeter(){
+            return "Ошибка! Треугольник не существует";
+          },
+          get area(){
+            return "Ошибка! Треугольник не существует";
+          }
+        }
+      }
+    }
